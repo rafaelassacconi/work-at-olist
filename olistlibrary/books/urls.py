1bbaf176f41675b51813b159fba14c2ef254bcaf
Rafaela Sacconi <rafaelassacconi@gmail.com>
2020-04-20 15:08:31 -0300

    Test redirect URL on Heroku

--- olistlibrary/books/urls.py
+++ olistlibrary/books/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^v1/', include('books.api.v1.urls')),
-    url(r'', RedirectView.as_view(url='/v1')),
+    url(r'^$', RedirectView.as_view(url='/v1/', permanent=False)),
 ]
